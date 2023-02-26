8f08d055b16d5c1d116f1387c0df1d3cda87bbb4
rodionion <rodionvh@gmail.com>
2023-02-26 10:32:59 -0300

    Add colors and hide header until scroll

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -8,10 +8,12 @@ import "../styles/style-products-specific.css";
 import "../styles/style-index.css";
 import "../graphics/logo.png"
 
-
 // 	------------------------------
 // 	Header
 // 	------------------------------
+
+console.log("1234");
+
 
 class header extends HTMLElement {
     constructor() {
@@ -23,14 +25,11 @@ class header extends HTMLElement {
         <header>
 
             <!-- Header Logo -->
-
             <a href="./index.html">
                 <img id="logo" src="./logo.png" alt="kalahari-biocare-logo"></a>
 
             <!-- Header hamburger icon -->
-
             <input type="checkbox" id="hamburger-checkbox">
-            
             <label id="hamburger" for="hamburger-checkbox">
                 <span class="hamburgerLine" id="hamburgerLine-1"></span>
                 <span class="hamburgerLine" id="hamburgerLine-2"></span>
@@ -38,38 +37,24 @@ class header extends HTMLElement {
             </label>
 
             <!-- Header menu links -->
-
             <nav id="headerMenu">
-
                 <ul>
-
                     <li><a class="headerMenu-link" href="./products.html">Products</a></li>
-
                     <li><a class="headerMenu-link" href="./contact.html">Contact</a></li>
-
                     <li><a class="headerMenu-link" href="./about.html">About</a></li>
-
                     <li><a class="headerMenu-link" href="./blog.html">Blog</a></li>
-
-                    
-            
                 </ul>
-            
             </nav>
-            
         </header>
         `;
     }
 }
 
-/*
-
-<li><a class="headerMenu-link" href="https://kalaharibiocare.com/contact">Contact</a></li>
-
-                    <li><a class="headerMenu-link" href="https://kalaharibiocare.com/about">About</a></li>
-
-                    <li><a class="headerMenu-link" href="https://kalaharibiocare.com/blog">Blog</a></li>
-
-*/
-
 customElements.define("header-component", header);
+
+
+// Reveal header on scroll down
+const headerElement = document.querySelector("header");
+document.addEventListener("scroll", 
+    () => headerElement.classList.add("headerAfterScroll"), 
+    { once: true });
