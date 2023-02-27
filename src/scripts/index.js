4ab0bc9d012f3590e38d0092b13073d364bac12f
rodionion <rodionvh@gmail.com>
2023-02-27 13:35:55 -0300

    Add reveal animation to Beliefs section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
-document.addEventListener("scroll", revealWhatsAppButton);
+// Reveal WhatsApp button on scroll
 
+document.addEventListener("scroll", revealWhatsAppButton);
 function revealWhatsAppButton() {
     console.log(window.scrollY);
     const floatingWhatsAppButton = document.querySelector(".floatingWhatsAppButton");
@@ -8,3 +9,29 @@ function revealWhatsAppButton() {
         document.removeEventListener("scroll", revealWhatsAppButton);
     }
 }
+
+// Reveal divs in Beliefs section
+
+function scrollTrigger(selector, options = {}) {
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+      addObserver(el, options)
+    })
+  }
+
+  function addObserver(el, options) {
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add('active')
+          observer.unobserve(entry.target)
+        }
+      })
+    }, options)
+    observer.observe(el)
+  }
+
+  scrollTrigger('.feature', {
+    rootMargin: '-200px'
+  })
